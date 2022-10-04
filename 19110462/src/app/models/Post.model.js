eb6dcf452ffa169ecc1272208476a166e1346ed3
MinhThang03 <minhhoangthang75@gmail.com>
2022-10-04 23:50:09 +0700

    first commit

---
+++ 19110462/src/app/models/Post.model.js
@@ -0,0 +1,22 @@
+const Post = [
+  {
+    id: 1,
+    title: "Spring boot là gì?",
+    description:
+      "Spring Boot là một extension của Spring Framework giúp các lập trình viên loại bỏ các bước cấu hình phức tạp mà Spring bắt buộc. Spring Boot là dự án phát triển bởi ngôn ngữ Java (JAV) trong hệ sinh thái Spring framework."
+  },
+  {
+    id: 2,
+    title: "Java là gì",
+    description:
+      "Java là ngôn ngữ lập trình hướng đối tượng (OOP), đa mục đích và độc lập nền tảng. Code Java “viết một lần, chạy mọi nơi”, làm việc “lý tưởng” trên bất kì thiết bị chạy hệ điều hành nào, miễn là được cài đặt Java Runtime Environment (JRE)."
+  },
+  {
+    id: 3,
+    title: "MERN Stack là gì?",
+    description:
+      "Chắc mọi người cũng đã từng nghe qua MEAN stack. MERN stack là nguyên bộ combo open source các công nghệ đều liên quan đến Javascript là cũng hot nhất hiện nay: MongoDB, ExpressJS, React/React Native, NodeJS. Người ta dùng MERN stack để xây dựng React Universal App"
+  },
+];
+
+module.exports = Post;
